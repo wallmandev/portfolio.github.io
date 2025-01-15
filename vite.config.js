b0466d525d0dfdbc9a215f07976a06f898fdcfde
wallmandev <wallman92@gmail.com>
2025-01-15 12:22:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wallmandev.github.io', // Files are served from the root
+  base: '/wallmandev.github.io/', // Byt "portfolio" till namnet på ditt repository
 })
